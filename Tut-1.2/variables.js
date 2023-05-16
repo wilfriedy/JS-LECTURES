a26211a6e17b99eb65c9034ddc881f0e15a77591
wilfriedy <wilfriedakpe3@gmail.com>
2023-05-16 06:44:33 +0100

    let var and const

---
+++ Tut-1.2/variables.js
@@ -0,0 +1,42 @@
+// VAR
+// console.log(example);
+var example = "This is a var first";
+// console.log(example);
+
+function demoVar() {
+  var example2 = "This is a second one";
+  //   console.log(example2);
+  console.log(example);
+}
+// demoVar();
+// console.log(example2);
+
+if (true) {
+  var cond = "helllo";
+}
+// console.log(cond);
+
+//LET
+
+// if (true) {
+//   let letVariable = "helllo";
+// }
+
+// {
+//   let letVariable = "helllo";
+// }
+
+// console.log(letVariable);
+// let letVariable = "let variable";
+// console.log(letVariable);
+
+//CONST
+
+// const fruits = "Mango";
+// fruits = "Oranges";
+// console.log(fruits);
+
+const fruits = ["Mango", "oranges"];
+console.log(fruits);
+fruits.push("Avocado");
+console.log(fruits);
