b2d89108406b1707eea8b578efd8b645b2b3d4f5
wilfriedy <wilfriedakpe3@gmail.com>
2023-05-16 04:30:57 +0100

    operators

---
+++ Tut-1.1/app.js
@@ -0,0 +1,3 @@
+let dataTypes = [2132, "this is a str", false];
+console.log(dataTypes);
+console.log(typeof dataTypes);
---
+++ Tut-1.2/operators.js
@@ -0,0 +1,49 @@
+//📏JAVASCRIPT OPERATORS📐//
+// console.log()
+let log = console.log;
+
+//1️⃣ Arithmetic Operators (+ - * / % **)
+// let operation = 25 / 5;
+// console.log(operation);
+//2️⃣ Unary operators (+,-,++,--,!, typeof, delete)
+// let obj = { prop1: 1, prop2: 2 };
+// log(obj);
+// delete obj.prop1;
+// log(obj);
+// log(!true);
+// let unaryExample = -"5";
+// log(unaryExample);
+// log(typeof unaryExample);
+// let unaryExample = 5;
+// log(unaryExample--);
+// log(unaryExample);
+
+//3️⃣ Assignment Operators(=,+=, -=, *=, /= , %=, **=)
+let operation = 21;
+operation **= 3;
+// operation = operation + 3;
+// log(operation);
+
+//4️⃣ Comparison Operators (==, ===, !=, !==, >, <, >= , <=)
+//TYPE COERCION********
+// let y = 5;
+// let x = 20.1;
+// if (x <= 20) {
+//   log(true);
+// } else {
+//   log(false);
+// }
+// log(y !== x);
+
+//5️⃣ Logical Operators (&&, ||, !)
+// let y = 5;
+// let x = 6;
+// log(x > 0 && y > 4 && x === 6);
+// log(x > 10 || y < 4 || y == 7);
+
+//6️⃣ Ternary Operators (? :)
+
+let y = 5;
+let x = 6;
+let result = x < y ? "x is bigger" : "x is smaller";
+log(result);
