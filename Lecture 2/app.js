393fd275a38ba33d142a4b195ff3fdc96ec8d888
wilfriedy <wilfriedakpe3@gmail.com>
2023-05-18 21:11:14 +0100

    data  types lecture

---
+++ Lecture 2/app.js
@@ -0,0 +1,31 @@
+// **** 🤔🤔DECLARING VARIABLES🤔🤔****
+var box2 = "two";
+let box1 = "one";
+const box3 = "three";
+
+// **** 😎😎DATA TYPES😎😎****
+let aString = "String";
+// console.log(aString);
+let aNumber = 43;
+// console.log(aNumber);
+let aBoolean = true;
+// console.log(aBoolean);
+let anArray = ["1", 2];
+// console.log(anArray);
+function name() {}
+
+// ****🛑🛑RULES🛑🛑****
+
+//Use camelCase
+let userName = "John Doe";
+// console.log()
+// Be descriptive
+let emailAddress = "john@example.com";
+// console.log()
+// Do not start with a number
+let variable = "test";
+// console.log()
+let _privateVariable = "secret";
+// console.log()
+// Avoid reserved words
+// let function = "function"
