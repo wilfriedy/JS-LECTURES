d65ee7ff6667247e40e345d63b0e114e36315e3c
wilfriedy <wilfriedakpe3@gmail.com>
2023-05-19 04:48:31 +0100

    ternary and logical operators

--- Tut-1.3/operators.js
+++ Tut-1.3/operators.js
@@ -28,7 +28,38 @@ let log = console.log;
 // console.log(x);
 
 //4️⃣ Comparison Operators (==, ===, !=, !==, >, <, >= , <=)
+// type coercion
+// let x = 2;
+// let y = "4";
+// console.log(x <= y);
 
 //5️⃣ Logical Operators (&&, ||)
+//and or
+// undefined, "", -0 => falsy
+// 11, "0", [], {} => truthy
+
+// let a = undefined;
+// let b = 5;
+// console.log(a || b || -0);
+
+// let a = 10;
+// let b = 6;
+// let c = 5;
+// let d = 5;
+
+// console.log(a > b || c > d );
+// console.log(a > b && c > d );
 
 //6️⃣ Ternary Operators (? :)
+let a = 10;
+
+// if (a === 4) {
+//   console.log(true);
+//   console.log("not good");
+// } else {
+//   console.log(false);
+//   console.log(" good");
+// }
+
+// let answer =
+// console.log(a === 4 ? true  "two" : false);
