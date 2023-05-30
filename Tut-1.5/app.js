c7fe6dbdcca9250ee459a347c281439a59d77c65
wilfriedy <wilfriedakpe3@gmail.com>
2023-05-31 00:54:39 +0100

    comments

--- Tut-1.5/app.js
+++ Tut-1.5/app.js
@@ -1,41 +1,53 @@
-let day = 3;
-let dayName;
+// let day = 3;
+// let dayName;
 
-switch (day > 2) {
-  case 1:
-    dayName = "Monday";
-    break;
-  case 2:
-    dayName = "Tuesday";
-    break;
-  case 3:
-    dayName = "Wednesday";
-    // console.log("Here is a code");
-    break;
-  case 4:
-    dayName = "Thursday";
-    break;
-  case 5:
-    dayName = "Friday";
-    break;
-  case 6:
-    dayName = "Saturday";
-    break;
-  case 7:
-    dayName = "Sunday";
-    break;
-  default:
-    dayName = "Invalid day";
-}
+// switch (day > 2) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     // console.log("Here is a code");
+//     break;
+//   case 4:
+//     dayName = "Thursday";
+//     break;
+//   case 5:
+//     dayName = "Friday";
+//     break;
+//   case 6:
+//     dayName = "Saturday";
+//     break;
+//   case 7:
+//     dayName = "Sunday";
+//     break;
+//   default:
+//     dayName = "Invalid day";
+// }
 
-console.log(dayName);
+// console.log(dayName);
 
-let marksAge = false;
+// let marksAge = false;
 
-if (marksAge) {
-  console.log("marksAge is less than 10");
-} else if (marksAge < 20) {
-  console.log("marksAge is less than 20 but greater or equal to 10");
-} else {
-  console.log("marksAge is 20 or greater");
-}
+// if (marksAge) {
+//   console.log("marksAge is less than 10");
+// } else if (marksAge < 20) {
+//   console.log("marksAge is less than 20 but greater or equal to 10");
+// } else {
+//   console.log("marksAge is 20 or greater");
+// }
+
+// let someExpression = 4;
+// switch (someExpression) {
+//   case 4:
+//     // result of the expression matches value1
+//     break;
+//   case value2:
+//     // result of the expression matches value2
+//     break;
+//   default:
+//   // the values match the value of the expression
+// }
