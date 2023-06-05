4a3e976df9d71624e39ffc0f70a9d3814dde5ec3
wilfriedy <wilfriedakpe3@gmail.com>
2023-06-05 20:13:47 +0100

    functions part 1

---
+++ Tut-1.6/functions.js
@@ -0,0 +1,20 @@
+function printPresentation(firstname, surname) {
+  return "Hello my name is " + firstname + " " + surname;
+}
+
+let newPresentation = printPresentation("Ted", "Jack");
+
+console.log(newPresentation);
+
+
+
+
+
+
+
+
+
+
+
+
+
