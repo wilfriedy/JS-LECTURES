c57938976c44bd3d5556108a19b58bbff2c6d718
wilfriedy <wilfriedakpe3@gmail.com>
2023-06-05 23:45:08 +0100

    functions part 2

---
+++ Tut-1.6/function.js
@@ -0,0 +1,19 @@
+function giveInstructions(instruction) {
+  // code block
+  console.log(instruction);
+  //   console.log("before");
+  //   return instruction;
+  //   console.log("after");
+}
+let todo = function (order) {
+  console.log(order);
+  return order;
+};
+console.log(todo("wake up"));
+
+// let todo = giveInstructions("Hello world");
+// console.log(todo);
+
+giveInstructions("Hello world");
+// giveInstructions("about to give instruction");
+// giveInstructions(2387);
